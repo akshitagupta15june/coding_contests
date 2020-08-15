a475c96351129665e791d75e20c4e546ef0ec139
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-08-15 21:53:55 +0530

    Create Evaluate_expressions.cpp

---
+++ Evaluate_expressions.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+int main() {
+    long long int t;
+    long long int sum=0;
+    cin>>t;
+    while(t--)
+    {
+        
+        long long int n;
+        long long int power=0;
+        cin>>n;
+        long long int d=n%10;
+        n=n/10;
+        power=pow(n,d);
+        sum+=power;
+    }
+    cout<<sum<<endl;
+
+	// Write your code here
+}
